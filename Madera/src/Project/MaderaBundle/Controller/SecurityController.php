aea3de6dfb9295c6a9486e876f135da8166c690c
jordan_maccioni <jordan.maccioni@viacesi.fr>
2016-12-13 16:31:31 +0100

    Continuation du login...

--- Madera/src/Project/MaderaBundle/Controller/SecurityController.php
+++ Madera/src/Project/MaderaBundle/Controller/SecurityController.php
@@ -1,27 +1,24 @@
 <?php
+
 namespace Project\MaderaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Symfony\Component\Security\Core\SecurityContext;
 
 class SecurityController extends Controller
 {
-    public function loginAction(Request $request)
-    {
-
-	    $authenticationUtils = $this->get('security.authentication_utils');
-
-	    // get the login error if there is one
-	    $error = $authenticationUtils->getLastAuthenticationError();
-
-	    // last username entered by the user
-	    $lastUsername = $authenticationUtils->getLastUsername();
-
-	    return $this->render('security/login.html.twig', array(
-	        'last_username' => $lastUsername,
-	        'error'         => $error,
-	    ));
+  public function loginAction(Request $request)
+  {
+    // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+    if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+      return $this->redirectToRoute('madera_new_devis');
     }
+    $authenticationUtils = $this->get('security.authentication_utils');
+    
+    return $this->render('MaderaBundle:Devis:connexion.html.twig', array(
+      'last_username' => $authenticationUtils->getLastUsername(),
+      'error'         => $authenticationUtils->getLastAuthenticationError(),
+    ));
+  }
 }
